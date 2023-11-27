a5b138ebc7bfc400c6c94f04a219b9fee5f2ad10
Stefan Roets <stefanroetscode@gmail.com>
2023-11-27 15:42:41 +0200

    Deplay

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://Stefanroets180/My-First-Astro-Application.github.io',
+});
